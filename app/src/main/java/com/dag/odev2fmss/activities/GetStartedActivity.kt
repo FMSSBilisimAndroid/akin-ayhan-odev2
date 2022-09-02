2e18cc73045165d3a5180c0e10c369e6fd698122
akın ayhan <akinayhan94@gmail.com>
2022-09-02 12:09:17 +0300

    create activities

--- app/src/main/java/com/dag/odev2fmss/activities/GetStartedActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/activities/GetStartedActivity.kt
@@ -1,12 +1,22 @@
 package com.dag.odev2fmss.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.dag.odev2fmss.R
+import com.dag.odev2fmss.databinding.ActivityGetStartedBinding
 
 class GetStartedActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGetStartedBinding
+    private lateinit var loginIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_started)
+        loginIntent = Intent(this, LoginActivity::class.java)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_get_started)
+        binding.btnJoinNow.setOnClickListener {
+            startActivity(loginIntent)
+        }
     }
 }
--- app/src/main/java/com/dag/odev2fmss/activities/LoginActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/activities/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.dag.odev2fmss.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.dag.odev2fmss.R
+import com.dag.odev2fmss.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var singUpIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        singUpIntent = Intent(this,SingUpActivity::class.java)
+        binding.txtOrCreate.setOnClickListener(){
+            startActivity(singUpIntent)
+        }
     }
 }
