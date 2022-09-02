2721e9487d44013fcd5cadd6a627748cbc6a4e70
akın ayhan <akinayhan94@gmail.com>
2022-09-02 11:49:40 +0300

    add databinding

--- app/src/main/java/com/dag/odev2fmss/activities/LoginActivity.kt
+++ app/src/main/java/com/dag/odev2fmss/activities/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.dag.odev2fmss
+package com.dag.odev2fmss.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dag.odev2fmss.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/dag/odev2fmss/activities/SingUpActivity.kt
@@ -0,0 +1,12 @@
+package com.dag.odev2fmss.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dag.odev2fmss.R
+
+class SingUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+    }
+}
